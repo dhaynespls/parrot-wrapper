22a3f5fb1563c83411047eb975294d5068dcfb27
David Haynes <dhaynes3@gmu.edu>
2017-11-07 12:04:49 -0500

    Initial scaffolding
    - more gitignore
    - more words
    - more boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# parrot-wrapper
+# Parrot Wrapper
+
 A wrapper for the Parrot SDK in python.
+
+Python 3 with 2.7 backwards compat.
+
+## Setup
+
+```py
+pip install -r requirements.txt
+python parrot.py
+```
---
+++ parrot.py
@@ -0,0 +1,19 @@
+#!/bin/sh python
+"""
+parrot.py
+
+Python 3 with 2.7 backwards compat.
+"""
+# Future Imports
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+def main():
+    """
+    Call functions and alert the user as to what's going on.
+    """
+    print("Hello World")
+
+# If executed, run main()
+if __name__ == '__main__':
+    main()
